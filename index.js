f59223c2fc1c9f05d2ee825a7bbb437b2e9ec30b
unknown <mahamuda3052@gmail.com>
2022-11-26 06:55:06 +0600

    create specific categore clothe api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 const port =process.env.PORT || 5000;
 const app= express()
 require('dotenv').config()
@@ -16,11 +17,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
 try{
 const categoreCollection = client.db("usedClotheStore").collection("categores")
+const clothesCollection = client.db("usedClotheStore").collection("clothes")
+
 app.get('/categore',async(req,res)=>{
     const query = {}
     const result = await categoreCollection.find(query).toArray();
     res.send(result)
+});
+
+// get specific categore data
+app.get('/categore/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query={catagoreID:id}        
+    const categoreClothe = await clothesCollection.find(query).toArray();
+    res.send(categoreClothe)
 })
+
+
+// get specific id clothe 
+app.get('/clothe/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:ObjectId(id)}       
+    const clothe = await clothesCollection.findOne(query);
+    res.send(clothe)
+    // console.log(selecterClothe)
+})
+
+
 }
 finally{
 
